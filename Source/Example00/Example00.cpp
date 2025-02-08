b6fad8df3b7a301ca6703f2826e6de44e87d19dd
Meldak <meldak.z@gmail.com>
2025-02-08 13:07:07 -0600

    Initial commit | Example project

---
+++ Source/Example00/Example00.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Example00.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Example00, "Example00" );
